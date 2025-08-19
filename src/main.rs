8f25534dd2fe16d87199e3fd9b9fb436f5a6743f
Toby Faucher <toby@tfaucher.com>
2025-08-19 17:41:53 -0600

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("Hello, world!");
+    println!("This is a test to see if waka time is working because it seems that it is not but im not sure yet because we will see in the dashboard");
+}

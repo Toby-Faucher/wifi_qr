e0bb62e458c378fe22a85927b72dcc4f939bb282
Toby Faucher <toby@tfaucher.com>
2025-08-19 19:14:04 -0600

    Added the rest of the commands for the Generate command

--- src/main.rs
+++ src/main.rs
@@ -15,16 +15,41 @@ pub enum Commands {
     Generate {
 
         #[arg(short,long)]
-        ssid: String
+        ssid: String,
 
+        #[arg(short,long)]
+        password: Option<String>,
+
+        //TODO: Implement std::clone::Clone
+        // #[arg(short = 't',long, default_value = "wpa2")]
+        // security: SecurityType,
+
+        //TODO: Make file path type (or find one)
+        #[arg(short,long)]
+        output: Option<String>,
+        
+        #[arg(long,default_value = "256")]
+        size: u32,
+
+        #[arg(long)]
+        hidden: bool,
     }
 }
+
+
+pub enum SecurityType {
+    None,
+    Wep,
+    Wpa2,
+    Wpa3,
+}
+
 fn main() {
     let cli = Cli::parse();
 
     match cli.command {
-        Commands::Generate { ssid } => {
-            println!("Test with {}", ssid);
+        Commands::Generate { ssid, password, /* security, */ output, size, hidden} => {
+            //TODO: Input Validation
         }
     }
 }

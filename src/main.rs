122338ef164cb1bc3c49edc2be2c1d75cca97fd5
Toby Faucher <toby@tfaucher.com>
2025-08-19 18:55:48 -0600

    Basic Clie Structure Implemented

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
-use clap::{Parser, Command};
+use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
-struct Cli {}
+#[command(name = "wifi-qr")]
+#[command(about = "Generates Commands For WIFI Networks")]
+#[command(version)]
+struct Cli {
+    #[command(subcommand)]
+    pub command: Commands
+}
 
+
+#[derive(Subcommand)]
+pub enum Commands {
+    Generate {
+
+        #[arg(short,long)]
+        ssid: String
+
+    }
+}
 fn main() {
-    let _cli = Cli::parse();
-    println!("Hello World");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Generate { ssid } => {
+            println!("Test with {}", ssid);
+        }
+    }
 }
